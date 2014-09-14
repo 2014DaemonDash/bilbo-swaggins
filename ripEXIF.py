8140ffef7cf01769cc980999addf8b8b679149de
TFurman <tim.r.n.furman@gmail.com>
2014-09-13 22:45:24 -0400

    switched to "official" git repository

---
+++ ripEXIF.py
@@ -0,0 +1,20 @@
+from PIL import Image
+from PIL.ExifTags import TAGS
+import pprint
+ 
+def get_exif_data(fname):
+    """Get embedded EXIF data from image file."""
+    ret = {}
+    try:
+        img = Image.open(fname)
+        if hasattr( img, '_getexif' ):
+            exifinfo = img._getexif()
+            if exifinfo != None:
+                for tag, value in exifinfo.items():
+                    decoded = TAGS.get(tag, tag)
+                    ret[decoded] = value
+    except IOError:
+        print 'IOERROR ' + fname
+    return ret
+
+print get_exif_data("nonsense.jpg")
